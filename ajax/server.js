dc52f6e8a3a59df4b0cd964741138162e77519e7
FJeanC <jeanfagiani1@gmail.com>
2022-11-03 09:52:01 -0300

    fetch API - POST

--- ajax/server.js
+++ ajax/server.js
@@ -4,12 +4,12 @@ const app = express()
 
 
 app.use(express.static('.'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 const multer = require('multer')
 const storage = multer.diskStorage({
-    destination: function(req, file, callback) {
+    destination: function (req, file, callback) {
         callback(null, './upload')
 
     },
@@ -18,17 +18,23 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({storage}).single('arquivo')
+const upload = multer({ storage }).single('arquivo')
 
 app.post('/upload', (req, res) => {
-    upload(req, res, err  => {
-        if(err) {
+    upload(req, res, err => {
+        if (err) {
             return res.end('Ocorreu um erro')
         }
         res.end('Concluído com sucesso')
-        
+
     })
 })
 
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
 app.get('/teste', (req, res) => res.send('Ok'))
-app.listen(8080, ()=>console.log('Executando'))
+app.listen(8080, () => console.log('Executando'))
