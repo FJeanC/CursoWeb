34c4f8d99a215ba4ddf16302852bbf66f525a29c
FJeanC <jeanfagiani1@gmail.com>
2022-11-07 16:36:10 -0300

    Desenvolvimento & Produção

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,19 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
