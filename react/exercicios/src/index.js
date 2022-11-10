bb1d6f7e96fa8e469e4bd992f4f668220852702c
FJeanC <jeanfagiani1@gmail.com>
2022-11-10 13:23:27 -0300

    Componentes com parâmetro - React

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const elemento = <h1>React</h1>
-ReactDOM.render(elemento, document.getElementById('root'))
+import BomDia from './componentes/BomDia'
+
+// const elemento = <h1>React</h1>
+ReactDOM.render(<BomDia nome="Guilherme" />, document.getElementById('root'))
 
