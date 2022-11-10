b6f57807da5f8c4292eb73322724b80f7b77db23
FJeanC <jeanfagiani1@gmail.com>
2022-11-10 10:51:27 -0300

    limpando e formatando o códgio - projeto Galeria

--- bootstrap/projeto-galeria/src/js/core/includes.js
+++ bootstrap/projeto-galeria/src/js/core/includes.js
@@ -3,23 +3,24 @@ import $ from 'jquery'
 const loadHtmlSucessCallBacks = []
 
 export function onLoadHtmlSucess(callback) {
-    if(!loadHtmlSucessCallBacks.includes(callback)) {
+    if (!loadHtmlSucessCallBacks.includes(callback)) {
         loadHtmlSucessCallBacks.push(callback)
     }
 }
 function loadIncludes(parent) {
-    if(!parent) parent = 'body'
-    $(parent).find('[wm-include]').each(function(i, e) {
+    if (!parent) parent = 'body'
+    $(parent).find('[wm-include]').each(function (i, e) {
         const url = $(e).attr('wm-include')
         $.ajax({
             url,
             success(data) {
                 $(e).html(data),
-                $(e).removeAttr('wm-include')
+                    $(e).removeAttr('wm-include')
                 loadHtmlSucessCallBacks.forEach(callback => callback(data))
                 loadIncludes(e)
             }
         })
     })
 
-}loadIncludes()
+}
+loadIncludes()
--- bootstrap/projeto-galeria/src/js/plugins/cityButton.js
+++ bootstrap/projeto-galeria/src/js/plugins/cityButton.js
@@ -3,10 +3,10 @@ import { onLoadHtmlSucess } from '../core/includes'
 const duration = 600
 
 function filterByCity(city) {
-    $('[wm-city]').each(function(i, e) {
+    $('[wm-city]').each(function (i, e) {
         const isTarget = $(this).attr('wm-city') === city
             || city === null
-        if(isTarget) {
+        if (isTarget) {
             $(this).parent().removeClass('d-none')
             $(this).fadeIn(duration)
         } else {
@@ -16,32 +16,31 @@ function filterByCity(city) {
         }
     })
 }
-const cityButtons = $('[wm-city-buttons]')
 
-$.fn.cityButtons = function(){
+$.fn.cityButtons = function () {
 
-const cities = new Set
-$('[wm-city]').each(function(i, e) {
-    cities.add($(e).attr('wm-city'))
-})
+    const cities = new Set
+    $('[wm-city]').each(function (i, e) {
+        cities.add($(e).attr('wm-city'))
+    })
 
-const btns = Array.from(cities).map(city => {
-    const btn = $('<button>')
-        .addClass(['btn', 'btn-info']).html(city)
-    btn.on("click", e => filterByCity(city))
-    return btn
-})
+    const btns = Array.from(cities).map(city => {
+        const btn = $('<button>')
+            .addClass(['btn', 'btn-info']).html(city)
+        btn.on("click", e => filterByCity(city))
+        return btn
+    })
 
-const btnAll = $('<button>')
-    .addClass(['btn', 'btn-info', 'active']).html('Todas')
-btnAll.on("click", e => filterByCity(null))
-btns.push(btnAll)
+    const btnAll = $('<button>')
+        .addClass(['btn', 'btn-info', 'active']).html('Todas')
+    btnAll.on("click", e => filterByCity(null))
+    btns.push(btnAll)
 
-const btnGroup = $('<div>').addClass(['btn-group'])
-btnGroup.append(btns)
+    const btnGroup = $('<div>').addClass(['btn-group'])
+    btnGroup.append(btns)
 
-$(this).html(btnGroup)
-return this
+    $(this).html(btnGroup)
+    return this
 }
 
 onLoadHtmlSucess(function () {
