e598ff40c45888a91b7fcea056bfc17526682eae
FJeanC <jeanfagiani1@gmail.com>
2022-11-10 10:15:48 -0300

    Adicionando as fotos na página - Galeria

--- bootstrap/projeto-galeria/src/index.js
+++ bootstrap/projeto-galeria/src/index.js
@@ -1,3 +1,12 @@
 // Arquivo sass
 
 import './scss/index.scss'
+
+// Dependencias
+
+import 'jquery'  // Entende que é .js por padrão
+import 'bootstrap'
+
+// Meu arquivo js
+
+import './js/core/includes.js'
