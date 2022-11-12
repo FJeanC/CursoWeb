970e3081d85f00ed9d9e3e9eebe76657509ae6ce
FJeanC <jeanfagiani1@gmail.com>
2022-11-12 10:14:15 -0300

    CSS global e modularizado - NextJS

---
+++ nextJS/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,8 @@
+import styles from './Estiloso.module.css'
+export default function Estiloso() {
+    return (
+        <div>
+            <h1 className={styles.roxo}>Estilo usando CSS Módulos</h1>
+        </div>
+    )
+}
