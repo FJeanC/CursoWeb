26a704db1cf4295a7c4a1d1ed4352b3a20aafb52
FJeanC <jeanfagiani1@gmail.com>
2022-11-18 13:14:55 -0300

    Titulo e Layout - projeto NextJs-FireBase-Tailwind

--- nextJS/projeto/next-crud/src/pages/index.tsx
+++ nextJS/projeto/next-crud/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -8,7 +9,9 @@ export default function Home() {
       flex justify-center items-center h-screen
       bg-gradient-to-r from-blue-500 to-purple-500
     `}>
-     <h1>Apps</h1>
+    <Layout titulo='Cadastro Simples'>
+      <span>Conteudo</span>
+    </Layout>
     </div>
   )
 }
