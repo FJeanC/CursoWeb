d1ec897ee6a85b197f428323a1e34c59a841d1c9
FJeanC <jeanfagiani1@gmail.com>
2022-11-18 14:12:18 -0300

    Tabela+botao - projeto NextJs/Tailwind/Firebase

--- nextJS/projeto/next-crud/src/pages/index.tsx
+++ nextJS/projeto/next-crud/src/pages/index.tsx
@@ -1,17 +1,41 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Botao from '../components/Botao'
 import Layout from '../components/Layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 18, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4'),
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Exlcluir: ${cliente.nome}`)
+  }
   return (
     <div className={`
       flex justify-center items-center h-screen
       bg-gradient-to-r from-blue-500 to-purple-500
     `}>
-    <Layout titulo='Cadastro Simples'>
-      <span>Conteudo</span>
-    </Layout>
+      <Layout titulo='Cadastro Simples'>
+        <div className='flex justify-end'>
+          <Botao cor='green' className="mb-4">Novo Cliente</Botao>
+        </div>
+        <Tabela clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        />
+
+      </Layout>
     </div>
   )
 }
