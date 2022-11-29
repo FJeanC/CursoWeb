4b076b456215b1482f8f0fa442ce030b4af97059
FJeanC <jeanfagiani1@gmail.com>
2022-11-29 09:42:32 -0300

    Projeto Calculadora - Vue.js

---
+++ vue/calculator/src/main.js
@@ -0,0 +1,12 @@
+
+// import Vue from 'vue'
+// import App from './App'
+
+// new Vue({
+//     render: h => h(App)
+// }).$mount("#app")
+
+import { createApp} from 'vue'
+import App from './App.vue'
+
+createApp(App).mount("#app")
