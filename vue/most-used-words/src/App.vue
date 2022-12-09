32cc676ea14f95bd320f4e04b093752c8e4e3ef0
FJeanC <jeanfagiani1@gmail.com>
2022-12-09 09:53:44 -0300

    Interface Grafica - Projeto ElectronJS/VueJS

--- vue/most-used-words/src/App.vue
+++ vue/most-used-words/src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Most Used Words</v-toolbar-title>
+    </v-app-bar>
     <v-main>
-      <HelloWorld/>
+      <Home />
     </v-main>
   </v-app>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from "./components/HomeComponent.vue";
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-    HelloWorld,
+    Home
   },
-
   data: () => ({
     //
-  }),
-}
+  })
+};
 </script>
---
+++ vue/most-used-words/src/components/HomeComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container fluid>
+      <v-form>
+          <v-file-input 
+            label="Selecione as Legendas"
+            prepend-icon="mdi-message-text"
+            append-icon="mdi-send"
+            outlined
+            multiple
+            chips
+            v-model="files"
+            @click:append="processSubtitles" />
+      </v-form>
+      <div class="pills">
+          <Pill v-for="word in groupedWords" :key="word.name"
+            :name="word.name" :amount="word.amount" />
+      </div>
+  </v-container>
+</template>
+<script>
+import Pill from './PillComponent.vue'
+export default {
+  components: { Pill },
+  data: function () {
+    return {
+      files: [],
+      groupedWords: [
+        { name : 'he', amount: 1234},
+        { name : 'you', amount: 900},
+        { name : 'i', amount: 853},
+      ]
+    }
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files)
+    }
+  }
+}
+</script>
+
+<style>
+   .pills {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+</style>
