26fc3e3d84a08bf4aaff43ae54e9eecd1e6943c1
FJeanC <jeanfagiani1@gmail.com>
2022-12-13 10:50:20 -0300

    Componente product-create  - Projeto Angular9

--- angular/crud/frontend/src/app/app.module.ts
+++ angular/crud/frontend/src/app/app.module.ts
@@ -16,7 +16,9 @@ import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
-
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { MatButtonModule} from '@angular/material/button'
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +27,9 @@ import { RedDirective } from './directives/red.directive';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -35,6 +39,7 @@ import { RedDirective } from './directives/red.directive';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
+    MatButtonModule
 
   ],
   providers: [],
