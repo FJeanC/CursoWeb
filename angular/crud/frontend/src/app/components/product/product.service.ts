c9e807c61a4ba2460dea227981ee0a21f8351598
FJeanC <jeanfagiani1@gmail.com>
2022-12-14 16:27:05 -0300

    tabela de produtos e read-update- Projeto Angular9

--- angular/crud/frontend/src/app/components/product/product.service.ts
+++ angular/crud/frontend/src/app/components/product/product.service.ts
@@ -10,7 +10,7 @@ import { Product } from './product.model';
 export class ProductService {
 
   baseUrl = "http://localhost:3001/products"
-  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
   showMessage(msg: string): void {
     this.snackBar.open(msg, "X", {
@@ -24,7 +24,22 @@ export class ProductService {
     return this.http.post<Product>(this.baseUrl, product)
   }
 
-  read() :Observable<Product[]> {
+  read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl)
+  }
+
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
+
+  delete(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Product>(url)
   }
 }
