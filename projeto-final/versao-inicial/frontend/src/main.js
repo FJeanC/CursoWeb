adac8a9826f6133ac32b72fe5a2e53ada854753e
FJeanC <jeanfagiani1@gmail.com>
2022-12-23 09:28:15 -0300

    Home e página do ADM - Projeto Final

--- projeto-final/versao-inicial/frontend/src/main.js
+++ projeto-final/versao-inicial/frontend/src/main.js
@@ -7,6 +7,9 @@ import './config/bootstrap'
 import router from './config/router'
 Vue.config.productionTip = false
 
+//Temporario
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikpob24gRG9lIiwiZW1haWwiOiJqaG9uQGVtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NzE3OTc0NDQsImV4cCI6MTY3MjA1NjY0NH0.V_VByYn5oecQ079R4J66qfpFHQfZh2Zv99n_c8tBygs'
+
 new Vue({
   store,
   router,
