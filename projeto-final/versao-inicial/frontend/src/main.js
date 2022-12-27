8dd9f062f7749f90ac73624acf73c5a91125a7ee
FJeanC <jeanfagiani1@gmail.com>
2022-12-27 10:28:35 -0300

    Cadastro de Artigo - Projeto Final

--- projeto-final/versao-inicial/frontend/src/main.js
+++ projeto-final/versao-inicial/frontend/src/main.js
@@ -9,7 +9,7 @@ import './config/msg'
 Vue.config.productionTip = false
 
 //Temporario
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikpob24gRG9lIiwiZW1haWwiOiJqaG9uQGVtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NzE3OTc0NDQsImV4cCI6MTY3MjA1NjY0NH0.V_VByYn5oecQ079R4J66qfpFHQfZh2Zv99n_c8tBygs'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikpob24gRG9lIiwiZW1haWwiOiJqaG9uQGVtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NzIxNDYzNjAsImV4cCI6MTY3MjQwNTU2MH0.8jLAp9TXkbjDwgj2UM_Ez5jnHbv85wlYioN-PiGCkVI'
 
 new Vue({
   store,
